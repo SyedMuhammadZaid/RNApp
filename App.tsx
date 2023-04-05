acc1f3b381235a0736447dc8caa68206631b9c87
SyedMuhammadZaid <syedmuhammadzaid95@gmail.com>
2023-04-05 16:50:40 +0500

    last commit of a day,updated

--- App.tsx
+++ App.tsx
@@ -6,6 +6,9 @@ const App = () => {
 // use state
   const [name,setname] = useState('syed zaid');
   const [num,setnum] = useState(1);
+  const [showValue,setShow] = useState(false);
+  const [hidenum,sethide] = useState(false);
+  let data = "ahmed";
 
 //  on click button1
   const val = ()=>{
@@ -19,19 +22,32 @@ const App = () => {
 
 // button for state changed.
   const namechange = ()=>{
+    data = "akmal";
     setname('syed muhammad zaid');
   }
+
 // button for number updated. 
   const numchange = ()=>{
-    setnum(num+1);
+      setnum(num+1); 
+  }
+
+  const seenvalue = ()=>{
+    setShow(true);
+    sethide(false);
+  }
+
+  const hidevalue = ()=>{
+    setShow(false);
+    sethide(false);
   }
 
   return (
     <View style={{padding:10}}>
     <Header />
     {/* text  */}
-      <Text style={{fontSize:20, color:"white", margin:10}}>First Screen</Text>
+      <Text style={{fontSize:20, color:"white", margin:10}}>First Screen of react native.</Text>
       <Text style={{fontSize:20}}>{name}</Text>
+      <Text style={{fontSize:20}}>{data}</Text>
       <Text style={{fontSize:20}}>{num}</Text>
     {/* input text */}
       <TextInput placeholder="Type here..."/>
@@ -41,9 +57,19 @@ const App = () => {
       <Button title='value' color={'blue'} onPress={()=>{clicked_val("hello you clicked")}}/>
     {/* updating the state */}
     <Button title='Full name' onPress={namechange}></Button>
-    <Button title='number updated' onPress={numchange}></Button>
-      
+    <Button title='number updated' onPress={numchange}></Button> 
+
+    <View style={{borderColor:'red', marginTop:50, display:'flex', justifyContent:'center', alignItems:'center', gap:5}}>
+
+      {showValue && <Text style={{fontSize:30, fontWeight:700}}>{num}</Text>}
+      <Button title='display value' onPress={seenvalue}></Button>
+
+      {hidenum && <Text>{}</Text>}
+      <Button title='Hide value' onPress={hidevalue}></Button>
     </View>
+
+    </View>
+
   );
 };
 export default App;
