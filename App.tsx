69bd7feb860690683c833b2b585f8e44e99f5bbe
SyedMuhammadZaid <syedmuhammadzaid95@gmail.com>
2023-04-05 14:44:29 +0500

    new changes commited

--- App.tsx
+++ App.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import {Text, View, Button, TextInput} from 'react-native';
+import Header from './components/header';
 
 const App = () => {
+//  on click button1
+  const val = ()=>{
+      console.warn("button clicked")
+  }
 
-  const add = ()=>{
-      return  2 + 2;
+// on click button2
+  const clicked_val = (value:string)=>{
+    console.warn(value)
   }
 
   return (
-    <View>
-      <Text style={{fontSize:20, color:"white", margin:10}}>translate</Text>
-      <TextInput placeholder="Type here to translate!"/>
-      <Button title='translate'></Button>
-      <Text style={{fontSize:20}}>{add()}</Text>
-     
+    <View style={{padding:10}}>
+    <Header />
+    {/* text  */}
+      <Text style={{fontSize:20, color:"white", margin:10}}>First Screen</Text>
+    {/* input text */}
+      <TextInput placeholder="Type here..."/>
+    {/* button */}
+      <Button title='on press' color={'gray'} onPress={val}></Button>
+    {/* button that is sending a value on its on press */}
+      <Button title='value' color={'blue'} onPress={()=>{clicked_val("hello you clicked")}}/>
     </View>
   );
 };
---
+++ components/header.js
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+
+const Header = ()=>{
+    return(
+    <View style={{height:35, backgroundColor:"white", padding:3}}>
+        <Text style={{color:"gray", fontSize:20 , fontWeight:500}}>ABC company</Text>
+
+    </View>)
+}
+
+
+export default Header
