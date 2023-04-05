b93878e256589171bc5c028d2dd50fa163b8244f
SyedMuhammadZaid <syedmuhammadzaid95@gmail.com>
2023-04-05 15:49:36 +0500

    number button updated

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Text, View, Button, TextInput} from 'react-native';
 import Header from './components/header';
 
 const App = () => {
+// use state
+  const [name,setname] = useState('syed zaid');
+  const [num,setnum] = useState(1);
+
 //  on click button1
   const val = ()=>{
-      console.warn("button clicked")
+     console.warn('you clicked')
   }
 
 // on click button2
@@ -13,17 +17,32 @@ const App = () => {
     console.warn(value)
   }
 
+// button for state changed.
+  const namechange = ()=>{
+    setname('syed muhammad zaid');
+  }
+// button for number updated. 
+  const numchange = ()=>{
+    setnum(num+1);
+  }
+
   return (
     <View style={{padding:10}}>
     <Header />
     {/* text  */}
       <Text style={{fontSize:20, color:"white", margin:10}}>First Screen</Text>
+      <Text style={{fontSize:20}}>{name}</Text>
+      <Text style={{fontSize:20}}>{num}</Text>
     {/* input text */}
       <TextInput placeholder="Type here..."/>
     {/* button */}
       <Button title='on press' color={'gray'} onPress={val}></Button>
     {/* button that is sending a value on its on press */}
       <Button title='value' color={'blue'} onPress={()=>{clicked_val("hello you clicked")}}/>
+    {/* updating the state */}
+    <Button title='Full name' onPress={namechange}></Button>
+    <Button title='number updated' onPress={numchange}></Button>
+      
     </View>
   );
 };
