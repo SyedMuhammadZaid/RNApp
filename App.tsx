2a7f856afc2f5b277ac72fad5fde1cb431e88b0d
SyedMuhammadZaid <syedmuhammadzaid95@gmail.com>
2023-04-05 13:19:44 +0500

    added some jsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import {Text, View, Button} from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-// import HelloWorldApp from './helloworld';
+import {Text, View, Button, TextInput} from 'react-native';
+
 const App = () => {
+
+  const add = ()=>{
+      return  2 + 2;
+  }
+
   return (
     <View>
-      <Text style={{fontSize:20, color:"white", margin:10}}>Hello react native</Text>
-      <Button title='submit'></Button>
+      <Text style={{fontSize:20, color:"white", margin:10}}>translate</Text>
+      <TextInput placeholder="Type here to translate!"/>
+      <Button title='translate'></Button>
+      <Text style={{fontSize:20}}>{add()}</Text>
      
     </View>
   );
